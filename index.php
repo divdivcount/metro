8f36eda8547b037e730d118107dddaa37eea8bcc
spare8433 <thdqudcks2@gmail.com>
2021-03-29 01:38:51 +0900

    메인 수우우우우우정

--- index.php
+++ index.php
@@ -72,7 +72,6 @@ require_once('modules/db.php');
           <li>프로필</li>
         </ul> -->
       </div>
-      <div class="clear"></div>
 
 
     </div>
@@ -138,8 +137,7 @@ require_once('modules/db.php');
           <div class="imgbox_3"><img src="img\time.png" alt=""></div>
           <ul class="rightLine">
             <li><span>도착시간</span></li>
-            <li>메트로켓은 출발시간에 맞춰서 도착시간을 알려줍니다.</li>
-          </ul>
+            <li>메트로켓은 출발시간에 맞춰서 도착시간을 알려줍니다.</li>          </ul>
         </div>
 
         <div class="textBox_1">
@@ -166,47 +164,45 @@ require_once('modules/db.php');
     <!-- 푸터박스 -->
     <div id="footer_box">
       <div id="information_box">
-        <ul id="firstUl">
-          <li>
-            <div class="textStyle_2">
-              <span>상호</span> (주)메트로켓  <span>대표자명</span> 박제봉<br>
-              <span>사업자등록번호</span> 000-00-00000 <span>통신판매업신고번호</span> 제0000-성남-0000호<br>
-              <span>연락처</span> 00-000-0000  <span>팩스</span> 000-0000-0000  <span>이메일</span> jebong@metrocket.com<br>
-              <span>주소</span> 경기도 성남시 수정구 단대동 산성대로437번길 7  지하주차장&nbsp
-            </div>
-          </li>
 
-          <li>
-            <ul class="secondUl_1">
-              <li>상품등록</li>
-              <li>내 상품</li>
-              <li>채팅</li>
-              <li>프로필</li>
-            </ul>
-          </li>
+        <div class="item">
+          <span>상호</span> (주)메트로켓  <span>대표자명</span> 박제봉<br>
+          <span>사업자등록번호</span> 000-00-00000 <span>통신판매업신고번호</span> 제0000-성남-0000호<br>
+          <span>연락처</span> 00-000-0000  <span>팩스</span> 000-0000-0000  <span>이메일</span> jebong@metrocket.com<br>
+          <span>주소</span> 경기도 성남시 수정구 단대동 산성대로437번길 7  지하주차장
+        </div>
 
-          <li>
-            <ul class="secondUl_2">
-              <li>이용약관</li>
-              <li><a>개인정보처리방침</a></li>
-            </ul>
-          </li>
+        <div class="item">
+          <ul class="secondUl_1">
+            <li>상품등록</li>
+            <li>내 상품</li>
+            <li>채팅</li>
+            <li>프로필</li>
+          </ul>
+        </div>
 
-          <li>
-            <ul class="secondUl_3">
-              <li><img src="img/facebook.png" alt=""></li>
-              <li><img src="img/instagram.png" alt=""></li>
-              <li><img src="img/twitter.png" alt=""></li>
-              <li><img src="img/blog.png" alt=""></li>
-            </ul>
-          </li>
-
-          <li class="text" style="border-top: solid #5e5956 1px;margin:4.0rem 0;">
-            <div class="imgbox_4"><img src="img/metroket_footer.png" alt=""></div>
-          </li>
+      <div class="item">
+        <ul class="secondUl_2">
+          <li>이용약관</li>
+          <li><a href="">개인정보처리방침</a></li>
         </ul>
+      </div>
+
+      <div class="item">
+
+          <img src="img/facebook.png" alt="">
+          <img src="img/instagram.png" alt="">
+          <img src="img/twitter.png" alt="">
+          <img src="img/blog.png" alt="">
 
       </div>
+
+
+
+
+      </div>
+        <div class="imgbox_4"><img src="img/metroket_footer.png" alt=""></div>
+
       <div class="line">
 
       </div>
