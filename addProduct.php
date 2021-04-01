cced18b93798466882939fbd70efbc2b4cde2ddf
spare8433 <thdqudcks2@gmail.com>
2021-04-02 00:25:40 +0900

    0402 12:25 상품등록페이지 1차 완성

--- addProduct.php
+++ addProduct.php
@@ -26,7 +26,7 @@
       </div>
 
       <!-- 폼  -->
-      <form class="" action="addProduct.php" method="get">
+      <form class="" action="addProduct.php" method="post">
 
       <!-- 제목 입력 부분 -->
       <div id="insertTitle_box" class="radius_box" >
@@ -37,9 +37,9 @@
         </div>
 
         <div class="content_box">
-          <input class="w3-input" type="text">
+          <input id ="titleText" class="w3-input" type="text" name="title">
           <!-- php 추가예정  -->
-          <div id="checkTextSize">0/100</div>
+          <div id="check_TitleCount">0/100</div>
         </div>
 
       </div>
@@ -52,8 +52,9 @@
         </div>
 
         <div class="content_box">
-          <input class="w3-input" type="text">
+          <input id ="price" name="price" class="w3-input" type="text" placeholder="숫자만 입력해주세요." onkeyup="inputNumberFormat(this)">
           &#8361;
+
         </div>
       </div>
 
@@ -66,20 +67,20 @@
 
         <div class="content_box">
           <div class="categoryGrid_box">
-            <label><input type="radio" class="test" name="category" value="디지털/가전">디지털/가전</label>
-            <label><input type="radio" class="test" name="category" value="가구/인테리어">가구/인테리어</label>
-            <label><input type="radio" class="test" name="category" value="유아동/유아도서">유아동/유아도서</label>
-            <label><input type="radio" class="test" name="category" value="생활/가공식품">생활/가공식품</label>
-            <label><input type="radio" class="test" name="category" value="스포츠/레저">스포츠/레저</label>
-            <label><input type="radio" class="test" name="category" value="여성잡화">여성잡화</label>
-            <label><input type="radio" class="test" name="category" value="여성의류">여성의류</label>
-            <label><input type="radio" class="test" name="category" value="남성패션/잡화">남성패션/잡화</label>
-            <label><input type="radio" class="test" name="category" value="게임/취미">게임/취미</label>
-            <label><input type="radio" class="test" name="category" value="반려동물용품">반려동물용품</label>
-            <label><input type="radio" class="test" name="category" value="도서/티켓/음반">도서/티켓/음반</label>
-            <label><input type="radio" class="test" name="category" value="식물">식물</label>
-            <label><input type="radio" class="test" name="category" value="기타 중고물품">기타 중고물품</label>
-            <label><input type="radio" class="test" name="category" value="뷰티/미용">뷰티/미용</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="디지털/가전">디지털/가전</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="가구/인테리어">가구/인테리어</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="유아동/유아도서">유아동/유아도서</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="생활/가공식품">생활/가공식품</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="스포츠/레저">스포츠/레저</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="여성잡화">여성잡화</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="여성의류">여성의류</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="남성패션/잡화">남성패션/잡화</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="게임/취미">게임/취미</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="반려동물용품">반려동물용품</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="도서/티켓/음반">도서/티켓/음반</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="식물">식물</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="기타 중고물품">기타 중고물품</label>
+            <label id="" class="categorylabel"><input type="radio" class="test" name="category" value="뷰티/미용">뷰티/미용</label>
           </div>
         </div>
       </div>
@@ -88,26 +89,18 @@
       <div id="insertImg_box" class="radius_box">
         <div class="title_box">
           <h2><d>*</d>상품이미지 </h2>
-          <span> 최소 1개 이상의 이미지를 업로드 해주세요</span>
+          <span> 최소 1장 이상의 이미지를 업로드 해주세요 (최대 6장 이하 업로드 가능)</span>
         </div>
 
         <div class="content_box">
           <div class="imgGrid_box">
-            <div class="img_Item">
-              <span><d>*</d>메인 이미지</span>
-              <img class ="productImg" src="img\add_img.png" alt="">
-            </div>
-
-            <div class="img_Item">
-              <span>추가 이미지</span>
-              <img class ="productImg" src="img\add_img.png" alt="">
-            </div>
-
             <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
             <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
             <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
             <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
-            <input type="file" id="real-input" name="image" class="image_inputType_file" accept="img/*" style="display:none" required multiple>
+            <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
+            <div class="img_Item"><img class ="productImg" src="img\add_img.png" alt=""></div>
+            <input type="file" id="real-input" name="image[]" class="image_inputType_file" accept="img/*" style="display:none" required multiple>
 
           </div>
 
@@ -122,7 +115,8 @@
         </div>
 
         <div class="content_box">
-          <textarea name="text" rows="10" cols="80" style="width:100%" placeholder="내용을 작성해 상품을 소개해 주세요."></textarea>
+          <textarea id ="explainText" name="explainText" rows="16" cols="80" style="width:100%; resize: none"; placeholder="내용을 작성해 상품을 소개해 주세요."></textarea>
+          <div id="check_explainTextCount">0 / 1000</div>
         </div>
 
       </div>
@@ -140,10 +134,12 @@
     <?php require_once 'metrocket_footer.php';?>
   </body>
   <script type="text/javascript">
-    //const browseBtn = document.querySelector('.img_Item');
     const img_Item = document.getElementsByClassName('img_Item');
     const realInput = document.getElementById('real-input');
     const productImg = document.getElementsByClassName('productImg');
+
+
+    // 사진 업로드 관련 함수적용
     var img = document.getElementById('test');
     for (var i = 0; i < 5; i++) {
       img_Item.item(i).addEventListener('click',()=>{
@@ -151,11 +147,15 @@
      });
     }
 
-
+    //사진 업로드 함수
     realInput.addEventListener('change',function(){
       if (this.files.length > 6) {
         alert('사진은 6장만 등록이 가능합니다');
         realInput.value="";
+      }
+
+      for (var i = 0; i < 6; i++) {
+        productImg.item(i).src ="img/add_img.png";
       }
 
       for (var i = 0; i < this.files.length; i++) {
@@ -163,14 +163,68 @@
           alert('jpg 및 png 이미지를 업로드해주세요');
           realInput.value="";
         }else{
+
           productImg.item(i).src=URL.createObjectURL(this.files[i]);
         }
       }
     })
 
 
+    // 글자수 카운트 (상세설명쪽)
+    $('#explainText').keyup(function (e){
+    var content = $(this).val();
+    $('#check_explainTextCount').html(content.length+" / 1000");    //글자수 실시간 카운팅
+
+      if (content.length > 1000){
+          alert("최대 1000자까지 입력 가능합니다.");
+          $(this).val(content.substring(0, 1000));
+          $('#check_explainTextCount').html("1000 / 1000");
+      }
+    });
+
+    // 글자수 카운트 (제목쪽)
+    $('#titleText').keyup(function (e){
+    var content = $(this).val();
+    $('#check_TitleCount').html(content.length+" / 100");    //글자수 실시간 카운팅
+
+      if (content.length > 100){
+          alert("최대 100자까지 입력 가능합니다.");
+          $(this).val(content.substring(0, 100));
+          $('#check_TitleCount').html("100 / 100");
+      }
+    });
+
+    const categorylabel = document.getElementsByClassName('categorylabel');
 
 
+    for (var i = 0; i < 14; i++) {
+
+      categorylabel.item(i).addEventListener('click',(event)=>{
+        for (var j = 0; j < 14; j++) {
+            categorylabel.item(j).style.color='#3b3b3b';
+        }
+        event.target.parentNode.style.color='#0099ff';
+     });
+    }
+
+    //
+    // $('#price').keyup(function (e) {
+    //   var content = $(this).val();
+    //   alert($('#price').value);
+    //   $('#price').value = content.toLocaleString( 'ko-KR', { style: 'currency', currency: 'KRW' } );
+    // })
+
+    function inputNumberFormat(obj) {
+    obj.value = comma(uncomma(obj.value));
+    }
+    function comma(str) {
+        str = String(str);
+        return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+    }
+    function uncomma(str) {
+        str = String(str);
+        return str.replace(/[^\d]+/g, '');
+    }
 
 
   </script>
