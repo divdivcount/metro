83e850e029c292ebaf826e1014fd9d82ae602fa8
spare8433 <thdqudcks2@gmail.com>
2021-04-06 16:51:58 +0900

    긴급탈출 넘버투

--- member_update.php
+++ member_update.php
@@ -53,6 +53,33 @@ if(isset($_SESSION['ss_mb_id'])){
   .w3-container{background-color: #fff;}
   .button_contatiner_margin{padding-top: 88px; padding-bottom: 190px;}
   .p_container_margin{margin-bottom: 50px; margin-left: 70px;}
+
+  #selectMetro_box{
+    display: none;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #f9f9f9;
+    box-shadow: 10px 10px 50px 0 rgba(0, 0, 0, 0.16);
+    width: 100%;
+    height: 200px;
+    z-index: 1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform:translate(-50%,-50%);
+    border-radius: 30px;
+    font-family: "NotoSansKR_r";
+    font-size:2.3rem;
+  }
+
+
+  #selectMetro_box span{font-size:1.8rem;color: #a1a1a1;}
+  #selectMetro_box .find_item{width: 40%;border-bottom: 3px solid #a5a5a5;}
+  #selectMetro_box .w3-button{width:auto;height: 7.0rem;font-family: "NotoSansKR_r";}
+  #selectMetro_box .w3-input{outline: none;background-color:#f9f9f9!important;border: none;}
+  #selectMetro_box .w3-select{outline: none;background-color:#f9f9f9!important;border: none;}
+  #selectMetro_box .find_item:nth-child(1){}
+  #selectMetro_box #bothFind_item{display: flex;width: 60%;flex-direction: row;justify-content: space-between;align-items: center;}
 </style>
 </head>
 <body>
@@ -88,6 +115,90 @@ if(isset($_SESSION['ss_mb_id'])){
         <input class="input_email" type="text" id="email" name="mb_email" value="<?=$mb["mb_email"] ? $mb["mb_email"] : $om["om_email"] ?>"  readonly required>
       </p>
       </form>
+
+      <form  id="selectMetro_box" action="My_one_page.php" method="post">
+        <div id="bothFind_item">
+
+        <div class="find_item">
+          <span>호선을 선택해 주세요.</span>
+          <select name="ctg_name" id="selectID" class="w3-select">
+            <option value="">선택</option>
+            <?php
+            $sql = " select * from line";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+            <option value="<?=$row["l_id"]?>"><?=$row["l_name"]?></option>
+          <?php }
+          mysqli_close($conn);
+          ?>
+          </select>
+        </div>
+
+        <div class="find_item">
+          <span>지하철역을 입력해주세요.</span>
+          <script type="text/javascript">
+
+          $(document).ready(function(){ // html 문서를 다 읽어들인 후
+              $('#selectID').on('change', function(){
+                  if(this.value !== ""){
+                      var optVal = $(this).find(":selected").val();
+                      //alert(optVal);
+                      $.post('autosearch.php',{optVal:optVal}, function(data) {
+                        let source = $.map($.parseJSON(data), function(item) { //json[i] 번째 에 있는게 item 임.
+                          chosung = "";
+                          //Hangul.d(item, true) 을 하게 되면 item이 분해가 되어서
+                          //["ㄱ", "ㅣ", "ㅁ"],["ㅊ", "ㅣ"],[" "],["ㅂ", "ㅗ", "ㄲ"],["ㅇ", "ㅡ", "ㅁ"],["ㅂ", "ㅏ", "ㅂ"]
+                          //으로 나오는데 이중 0번째 인덱스만 가지고 오면 초성이다.
+                          full = Hangul.disassemble(item).join("").replace(/ /gi, "");	//공백제거된 ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ
+                          Hangul.d(item, true).forEach(function(strItem, index) {
+
+                            if(strItem[0] != " "){	//띄어 쓰기가 아니면
+                              chosung += strItem[0];//초성 추가
+
+                            }
+                          });
+
+
+                          return {
+                            label : chosung + "|" + (item).replace(/ /gi, "") +"|" + full, //실제 검색어랑 비교 대상 ㄱㅊㅂㅇㅂ|김치볶음밥|ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ 이 저장된다.
+                            value : item,
+                            chosung : chosung,
+                            full : full
+                          }
+                        });
+                        $("#auto").autocomplete({
+                                  source : source,	// source 는 자동 완성 대상
+                                  select : function(event, ui) {	//아이템 선택시
+                                    console.log(ui.item.label + " 선택 완료");
+
+                                  },
+                                  focus : function(event, ui) {	//포커스 가면
+                                    return false;//한글 에러 잡기용도로 사용됨
+                                  },
+                        }).autocomplete( "instance" )._renderItem = function( ul, item ) {
+                        //.autocomplete( "instance" )._renderItem 설절 부분이 핵심
+                            return $( "<li>" )	//기본 tag가 li로 되어 있음
+                              .append( "<div>" + item.value + "</div>" )	//여기에다가 원하는 모양의 HTML을 만들면 UI가 원하는 모양으로 변함.
+                              .appendTo( ul );	//웹 상으로 보이는 건 정상적인 "김치 볶음밥" 인데 내부에서는 ㄱㅊㅂㅇㅂ,김치 볶음밥 에서 검색을 함.
+                         };
+                  })
+                }
+              })
+            });
+            $("#auto").on("keyup",function(){	//검색창에 뭔가가 입력될 때마다
+            input = $("#auto").val();	//입력된 값 저장
+            $( "#auto" ).autocomplete( "search", Hangul.disassemble(input).join("").replace(/ /gi, "") );	//자모 분리후 띄어쓰기 삭제
+            })
+          </script>
+          <div style="display:flex"><input id="auto" class="w3-input highlight" value='' type="text"><div style="width:1.3rem;margin:auto"><img src="img\loupe.png" alt=""></div></div>
+        </div>
+
+      </div>
+
+      <button type="submit" id ="close_pop" class="w3-button w3-blue w3-ripple w3-round-xxlarge" name="button">물건보러가기</button>
+      </form>
+
       <p class="p_container_margin">
         <label>*주변 역 설정하기</label>
         <input class="input_station" type="text" id="pw2" required>	<button type="submit" id="joinBtn" class="w3-button w3-tiny w3-light-gray w3-round">역 검색</button>
@@ -99,5 +210,25 @@ if(isset($_SESSION['ss_mb_id'])){
   </div>
 </div>
 </body>
+<script type="text/javascript">
+$(document).ready(function(){
+  popup();
+
+  function popup(){
+    open_pop();
+    close_pop();
+  }
+  function open_pop(){
+    $('#joinBtn').click(function(){
+      $('#selectMetro_box').css({'display':'flex'});
+    });
+  }
+  function close_pop(){
+    $('#close_pop').click(function(){
+      $('#selectMetro_box').css({'display':'none'});
+    });
+  }
+});
+</script>
 </html>
 <?php } ?>
--- searchProduct.php
+++ searchProduct.php
@@ -1,3 +1,6 @@
+<?php
+require_once('modules/db.php');
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,7 +10,14 @@
     <link rel="stylesheet" href="css/css_metrocket_footer.css">
     <link rel="stylesheet" href="css/css_metrocket_header.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/hangul-js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <style>
+      .ui-helper-hidden-accessible{display:none;}
+    </style>
     <meta charset="utf-8">
     <title></title>
   </head>
@@ -46,9 +56,98 @@
 
         <!-- 호선 선택 버튼 -->
         <div class="btn_box">
-          <input class="w3-button w3-round-large" type="button" name="" value="호선선택">
+          <input id="pop" class="w3-button w3-round-large" type="button" name="" value="호선선택">
         </div>
+
+
+        <form  id="selectMetro_box" action="My_one_page.php" method="post">
+          <div id="bothFind_item">
+
+          <div class="find_item">
+            <span>호선을 선택해 주세요.</span>
+            <select name="ctg_name" id="selectID" class="w3-select">
+              <option value="">선택</option>
+              <?php
+              $sql = " select * from line";
+              $result = mysqli_query($conn, $sql);
+              while ($row = mysqli_fetch_assoc($result)) {
+              ?>
+              <option value="<?=$row["l_id"]?>"><?=$row["l_name"]?></option>
+            <?php }
+            mysqli_close($conn);
+            ?>
+            </select>
+          </div>
+
+          <div class="find_item">
+            <span>지하철역을 입력해주세요.</span>
+            <script type="text/javascript">
+
+            $(document).ready(function(){ // html 문서를 다 읽어들인 후
+                $('#selectID').on('change', function(){
+                    if(this.value !== ""){
+                        var optVal = $(this).find(":selected").val();
+                        //alert(optVal);
+                        $.post('autosearch.php',{optVal:optVal}, function(data) {
+                          let source = $.map($.parseJSON(data), function(item) { //json[i] 번째 에 있는게 item 임.
+                            chosung = "";
+                            //Hangul.d(item, true) 을 하게 되면 item이 분해가 되어서
+                            //["ㄱ", "ㅣ", "ㅁ"],["ㅊ", "ㅣ"],[" "],["ㅂ", "ㅗ", "ㄲ"],["ㅇ", "ㅡ", "ㅁ"],["ㅂ", "ㅏ", "ㅂ"]
+                            //으로 나오는데 이중 0번째 인덱스만 가지고 오면 초성이다.
+                            full = Hangul.disassemble(item).join("").replace(/ /gi, "");	//공백제거된 ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ
+                            Hangul.d(item, true).forEach(function(strItem, index) {
+
+                              if(strItem[0] != " "){	//띄어 쓰기가 아니면
+                                chosung += strItem[0];//초성 추가
+
+                              }
+                            });
+
+
+                            return {
+                              label : chosung + "|" + (item).replace(/ /gi, "") +"|" + full, //실제 검색어랑 비교 대상 ㄱㅊㅂㅇㅂ|김치볶음밥|ㄱㅣㅁㅊㅣㅂㅗㄲㅇㅡㅁㅂㅏㅂ 이 저장된다.
+                              value : item,
+                              chosung : chosung,
+                              full : full
+                            }
+                          });
+                          $("#auto").autocomplete({
+                                    source : source,	// source 는 자동 완성 대상
+                                    select : function(event, ui) {	//아이템 선택시
+                                      console.log(ui.item.label + " 선택 완료");
+
+                                    },
+                                    focus : function(event, ui) {	//포커스 가면
+                                      return false;//한글 에러 잡기용도로 사용됨
+                                    },
+                          }).autocomplete( "instance" )._renderItem = function( ul, item ) {
+                          //.autocomplete( "instance" )._renderItem 설절 부분이 핵심
+                              return $( "<li>" )	//기본 tag가 li로 되어 있음
+                                .append( "<div>" + item.value + "</div>" )	//여기에다가 원하는 모양의 HTML을 만들면 UI가 원하는 모양으로 변함.
+                                .appendTo( ul );	//웹 상으로 보이는 건 정상적인 "김치 볶음밥" 인데 내부에서는 ㄱㅊㅂㅇㅂ,김치 볶음밥 에서 검색을 함.
+                           };
+                    })
+                  }
+                })
+              });
+              $("#auto").on("keyup",function(){	//검색창에 뭔가가 입력될 때마다
+              input = $("#auto").val();	//입력된 값 저장
+              $( "#auto" ).autocomplete( "search", Hangul.disassemble(input).join("").replace(/ /gi, "") );	//자모 분리후 띄어쓰기 삭제
+              })
+            </script>
+            <div style="display:flex"><input id="auto" class="w3-input highlight" value='' type="text"><div style="width:1.3rem;margin:auto"><img src="img\loupe.png" alt=""></div></div>
+          </div>
+
+        </div>
+
+        <button type="submit" id ="close_pop" class="w3-button w3-blue w3-ripple w3-round-xxlarge" name="button">물건보러가기</button>
+        </form>
       </div>
+
+
+
+
+
 
       <!-- n 호선 및  n 역 상품 타이틀 -->
       <div id="productTitle">
@@ -132,4 +231,24 @@
     <?php require_once('metrocket_footer.php');?>
 
   </body>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      popup();
+
+      function popup(){
+        open_pop();
+        close_pop();
+      }
+      function open_pop(){
+        $('#pop').click(function(){
+          $('#selectMetro_box').css({'display':'flex'});
+        });
+      }
+      function close_pop(){
+        $('#close_pop').click(function(){
+          $('#selectMetro_box').css({'display':'none'});
+        });
+      }
+  });
+  </script>
 </html>
