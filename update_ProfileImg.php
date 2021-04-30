e236534cfd99172261e5f171225dfaecbd064b04
spare8433 <thdqudcks2@gmail.com>
2021-05-01 01:45:33 +0900

    유수정

--- update_ProfileImg.php
+++ update_ProfileImg.php
@@ -28,6 +28,17 @@
   }
 
 
-
-
  ?>
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      <script type="text/javascript">
+        alert("프로필 정보가 성공적으로 저장되었습니다");
+        hitory.back();
+      </script>
+    </body>
+  </html>
