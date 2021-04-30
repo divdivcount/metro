df6694d13db91a598a3030241cefedca4e2c5dba
spare8433 <thdqudcks2@gmail.com>
2021-05-01 01:47:34 +0900

    송수정

--- update_ProfileImg.php
+++ update_ProfileImg.php
@@ -4,7 +4,6 @@
   $member_num = $_POST["member_num"];
 
   if ($_FILES['files']['name'] != "") {
-    echo "1<br>";
     if($_FILES['files']['type'] == 'image/jpeg' || $_FILES['files']['type'] == 'image/png' || $_FILES['files']['type'] == 'image/gif') {
       $productIMG = new Member();
       $userprofile_img[] = $productIMG->fileUploader($_FILES['files']);
@@ -15,16 +14,12 @@
 
       $query = "update member set mb_image='$fileName' where mb_num = TRIM($member_num)";
       $result = mysqli_query($conn, $query);
-      echo "2<br>";
     }
   }else{
     $productIMG = new Member();
-    echo "3<br>";
     $query = "update member set mb_image = 'img/normal_profile.png' where mb_num = TRIM($member_num)";
     $result = mysqli_query($conn, $query);
-    echo "4<br>";
     $productIMG->Delete_mbImg($member_num);
-    echo "5<br>";
   }
 
 
