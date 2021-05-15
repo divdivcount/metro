3f044b8d8d0a0d3a8fe4013951bfb5963c662fba
divdivcount <jang05812002@naver.com>
2021-05-15 23:21:22 +0900

    난 안돼

--- memo_form.php
+++ memo_form.php
@@ -8,43 +8,45 @@ $pr_id = $_GET['id']; // GET 방식으로 넘어온 받는 제품번호
 <html>
 <head>
 	<title>Memo Form</title>
-	<link href="./style.css" rel="stylesheet" type="text/css">
+	  <link rel="stylesheet" href="css/note-3.css">
 </head>
 <body id="memo">
 	<!-- 쪽지 보내기 시작 { -->
+	<div class="note">
+    <div class="header">
+        <div>
+            <img src="img/note.png">
+            <span class="title">쪽지 보내기</span>
+
+        </div>
+    </div>
+			<button class="btn1" onclick="location.href ='./memo.php?kind=recive'">받은쪽지</button>
+			<button class="btn2" onclick="location.href ='./memo.php?kind=send'">보낸쪽지</button>
+	</div>
 	<div>
-		<h1>쪽지 보내기</h1>
 
-		<ul>
-			<li><a href="./memo.php?kind=recive">받은쪽지</a></li>
-			<li><a href="./memo.php?kind=send">보낸쪽지</a></li>
-		</ul>
-
+<div class="note2">
 		<form name="fmemoform" action="./memo_form_update.php" onsubmit="return fmemoform_submit(this);" method="post" autocomplete="off">
 			<input type="hidden" name= "id" value="<?=$pr_id?>">
 		<div>
-			<table>
-			<tbody>
-			<tr>
-				<th>받는 회원아이디</th>
-				<td>
-					<input type="text" name="me_recv_mb_id" value="<?php echo $me_recv_mb_id ?>" id="me_recv_mb_id" readonly required class="frm_input required" size="47"></br>
-				</td>
-			</tr>
-			<tr>
-				<th>내용</th>
-				<td><textarea name="me_memo" rows="10" cols="50" required></textarea></td>
-			</tr>
-			</tbody>
-			</table>
-		</div>
 
-		<div class="win_btn">
-			<input type="submit" value="보내기">
-			<button type="button" onclick="window.close();">창닫기</button>
+				<div class="content">
+            <table>
+                <tr>
+                    <td>보낸사람</td>
+                    <td><input type="text" name="me_recv_mb_id" value="<?php echo $me_recv_mb_id ?>" id="me_recv_mb_id" readonly required class="frm_input required" size="47"></td>
+                </tr>
+            </table>
+        </div>
+				<div class="conbtn">
+            <input type="submit" class="cbtn" value="답장하기">
+        </div>
 		</div>
+</div>
+<td><textarea name="me_memo" rows="10" cols="50" required></textarea></td>
 		</form>
 	</div>
+
 	<!-- 쪽지 보내기 끝 { -->
 </body>
 </html>
