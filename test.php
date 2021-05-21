e8995236b10611db06ad47638437c865ff1f61a0
divdivcount <jang05812002@naver.com>
2021-05-22 01:08:23 +0900

    dq

--- test.php
+++ test.php
@@ -10,11 +10,11 @@
   if(!(is_null($selectId))){
     $other_member = $dao->admin_Om_select($selectId);
       if(!(is_null($other_member))){
-        $member_check = "om";
+        $member_check = "om_id";
         $product->product_history_update($pr_id,$other_member[0]["om_id"], $member_check);
       }
     if(is_null($other_member)){
-      $member_check = "mb";
+      $member_check = "mb_id";
       $dao = new Member;
       $member = $dao->admin_Member_id_all_select($selectId);
           if(!(is_null($member))){
@@ -22,11 +22,11 @@
           }
       // var_dump($member);
       if(is_null($member)){
-        $member_check = "om";
+        $member_check = "om_id";
         $dao = new Oauths;
         $other_member = $dao->admin_Om_select($selectId);
         if(!(is_null($other_member))){
-          $member_check = "om";
+          $member_check = "om_id";
           $product->product_history_update($pr_id,$other_member[0]["om_id"], $member_check);
         }
       }else{
